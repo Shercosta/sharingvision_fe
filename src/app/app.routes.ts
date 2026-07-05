a0df56c1ca9ef366c9129413582634b28ae54334
Shercosta <shercostagp@gmail.com>
2026-07-05 17:30:34 +0700

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Preview } from './pages/preview/preview';
+import { AllPosts } from './pages/all-posts/all-posts';
+import { AddPost } from './pages/add-post/add-post';
+import { EditPost } from './pages/edit-post/edit-post';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'preview',
+    pathMatch: 'full',
+  },
+  {
+    path: 'preview',
+    component: Preview,
+  },
+  {
+    path: 'posts',
+    component: AllPosts,
+  },
+  {
+    path: 'posts/new',
+    component: AddPost,
+  },
+  {
+    path: 'posts/:id/edit',
+    component: EditPost,
+  },
+];
---
+++ src/app/pages/all-posts/all-posts.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-posts',
+  imports: [],
+  templateUrl: './all-posts.html',
+  styleUrl: './all-posts.css',
+})
+export class AllPosts {}
